be9cd6e24f298398fdab8dbd09c173f90a114eb5
Borja Muñoz <90717945+borjamrd@users.noreply.github.com>
2024-03-07 23:25:20 +0100

    chroe!: dummy data for json-server

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { MenubarModule } from 'primeng/menubar';
 @Component({
   selector: 'osea-header',
   standalone: true,
-  imports: [],
+  imports: [MenubarModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
