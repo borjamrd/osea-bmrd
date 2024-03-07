9f52f370fa8c52cb5e161c02fb72965dd67e6e85
Borja Muñoz <90717945+borjamrd@users.noreply.github.com>
2024-03-07 22:52:39 +0100

    config: paths added to tsconfig

---
+++ src/app/layout/layout/layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'osea-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.css'
+})
+export class LayoutComponent {
+
+}
