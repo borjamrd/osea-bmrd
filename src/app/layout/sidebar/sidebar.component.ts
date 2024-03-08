a6aa25eaff198e1afdb3a26002a12b80b1e8097d
Borja Muñoz <90717945+borjamrd@users.noreply.github.com>
2024-03-08 01:58:39 +0100

    styles: sidebar items created

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,19 +1,30 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { SidebarService } from '@app/shared/services/sidebar.service';
+import { ButtonModule } from 'primeng/button';
 import { SidebarModule } from 'primeng/sidebar';
 
 
 @Component({
   selector: 'osea-sidebar',
   standalone: true,
-  imports: [SidebarModule, CommonModule],
+  imports: [SidebarModule, CommonModule, ButtonModule, RouterModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
   sidebarVisible!: boolean
+
+  menuItems: { text: string, link: string }[] = [{ text: 'Canciones', link: 'songs' }, { text: 'Artistas', link: 'artists' }, {
+    text: 'Compañías discográficas', link: 'companies'
+  }]
+
+
+
   private _sidebar = inject(SidebarService)
+
+
 
   constructor() {
     this._sidebar.getStatus().subscribe((r) => this.sidebarVisible = r)
