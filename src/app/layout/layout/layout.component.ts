b2ec9dfb9c73f626d8012fec5247806352d4eeaa
Borja Muñoz <90717945+borjamrd@users.noreply.github.com>
2024-03-08 01:11:32 +0100

    chroe!: error-interceptor for http-requests

--- src/app/layout/layout/layout.component.ts
+++ src/app/layout/layout/layout.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
+import { FooterComponent } from '../footer/footer.component';
+import { HeaderComponent } from '../header/header.component';
 @Component({
   selector: 'osea-layout',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, HeaderComponent, ToastModule, ButtonModule, FooterComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
 export class LayoutComponent {
+
 
 }
