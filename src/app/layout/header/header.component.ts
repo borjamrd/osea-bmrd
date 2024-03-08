6bb8893623d7d3f987fda21023f70160e1d49c5e
Borja Muñoz <90717945+borjamrd@users.noreply.github.com>
2024-03-08 16:24:53 +0100

    chore!: language form added in header

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,17 +1,40 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SidebarService } from '@app/shared/services/sidebar.service';
 import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
 import { MenubarModule } from 'primeng/menubar';
+
+interface Language {
+  name: string;
+  code: string;
+}
 @Component({
   selector: 'osea-header',
   standalone: true,
-  imports: [MenubarModule, ButtonModule],
+  imports: [MenubarModule, ButtonModule, DropdownModule, ReactiveFormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  languages: Language[] = [
+    { name: 'Spain', code: 'ES' },
+    { name: 'United States', code: 'US' }
+  ];
   private _sidebar = inject(SidebarService)
+  private _fb = inject(FormBuilder)
+
+  languageForm
+
+  constructor() {
+
+    this.languageForm = this._fb.group({
+      selectedLang: new FormControl(this.languages[0])
+    })
+  }
+
+
+
   handleSidebar() {
 
     this._sidebar.changeOpenStatus(true)
