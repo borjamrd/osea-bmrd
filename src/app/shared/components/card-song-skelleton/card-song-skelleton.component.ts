77d3daa56cb706ade57336abdb8edd2ced054f88
Borja Muñoz <90717945+borjamrd@users.noreply.github.com>
2024-03-08 02:53:24 +0100

    displaying songs from service

---
+++ src/app/shared/components/card-song-skelleton/card-song-skelleton.component.ts
@@ -0,0 +1,13 @@
+import { SkeletonModule } from 'primeng/skeleton';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'osea-card-song-skelleton',
+  standalone: true,
+  imports: [SkeletonModule],
+  templateUrl: './card-song-skelleton.component.html',
+  styleUrl: './card-song-skelleton.component.css'
+})
+export class CardSongSkelletonComponent {
+
+}
